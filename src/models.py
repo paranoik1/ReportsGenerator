668cebf5cb09daf46d9f6709134824b18a79b613
paranoik1 <paranoik@noreplay.com>
2026-03-24 16:55:05 +0300

    template extractor html

--- src/models.py
+++ src/models.py
@@ -1,6 +1,11 @@
+import subprocess
 from dataclasses import dataclass, field
+from functools import cached_property
 from pathlib import Path
+from typing import Literal
 
+import structlog
+from bs4 import BeautifulSoup
 from pypandoc import convert_file
 from pypdf import PdfReader
 
@@ -9,17 +14,84 @@ from utils.data_block_registry import DataBlocksRegistry
 FilePath = str | Path
 
 
+logger = structlog.get_logger(__name__)
+
+
 @dataclass
 class Document:
     filepath: FilePath
-    content: str = field(default="", init=False)
+    extractor: Literal["default", "soffice"] = "default"
 
     def __post_init__(self):
-        self.content = self._extract_text()
+        self.filepath = Path(self.filepath)
+
+    @cached_property
+    def content(self):
+        return self._extract_text()
+
+    @staticmethod
+    def __clean_html(html: str) -> str:
+        soup = BeautifulSoup(html, "html.parser")
+
+        for tag in soup(["img", "head"]):
+            tag.decompose()
+
+        return soup.prettify()
+
+    def _soffice_extract_text(self) -> str:
+        # NOTE: For MYPY
+        if not isinstance(self.filepath, Path):
+            self.filepath = Path(self.filepath)
+
+        workdir = self.filepath.parent
+        cmd = f"soffice --headless --convert-to html:HTML:EmbedImages {self.filepath} --outdir {workdir}".split()
+
+        log = logger.bind(cmd=cmd)
+
+        try:
+            output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, text=True)
+        except subprocess.CalledProcessError as exc:
+            log.exception(
+                "failed_call_soffice", return_code=exc.returncode, output=exc.output
+            )
+            raise
+        else:
+            log.info("success_call_soffice", output=output)
+
+        html_filepath = self.filepath.with_suffix(".html")
+        with open(html_filepath) as fp:
+            content_html = fp.read()
+
+        cleaned_html = self.__clean_html(content_html)
+        cleaned_html_filepath = html_filepath.with_suffix(".clean.html")
+
+        with open(cleaned_html_filepath, "w") as fp:
+            fp.write(cleaned_html)
+
+        logger.info(
+            "clean_html",
+            content_cleaned_len=len(cleaned_html),
+            content_len=len(content_html),
+            cleaned_filepath=cleaned_html_filepath,
+        )
+
+        return cleaned_html
 
     def _extract_text(self) -> str:
         """Извлекает текст из файла в зависимости от расширения."""
-        ext = Path(self.filepath).suffix.lower()
+        # NOTE: For MYPY
+        if not isinstance(self.filepath, Path):
+            self.filepath = Path(self.filepath)
+
+        ext = self.filepath.suffix.lower()
+
+        if self.extractor == "soffice":
+            if ext not in {".docx", ".odt", ".doc"}:
+                raise ValueError(
+                    "Extractor soffice поддерживает только файлы документов"
+                )
+
+            return self._soffice_extract_text()
 
         if ext == ".pdf":
             reader = PdfReader(self.filepath)
@@ -30,7 +102,7 @@ class Document:
                 self.filepath, "markdown-simple_tables-grid_tables-multiline_tables"
             )
 
-        elif ext in [".txt", ".md"]:
+        elif ext in [".txt", ".md", ".html"]:
             with open(self.filepath, "r", encoding="utf-8") as f:
                 return f.read()
 
@@ -50,6 +122,7 @@ class ImageDocument:
 class StateAgents:
     task_id: str
     user_prompt: str
+    user_prompt_cleaned: str | None = None
 
     data_blocks_registry: DataBlocksRegistry = field(default_factory=DataBlocksRegistry)
     report_markdown: str | None = None
@@ -65,3 +138,11 @@ class StateAgents:
     documents: list[Document] = field(default_factory=list)
     template: Document | None = None
     images: list[ImageDocument] = field(default_factory=list)
+
+
+if __name__ == "__main__":
+    doc = Document(
+        "uploads/18ebf7db-284a-4f2a-b9a3-aed1ff5f0c10/template_10.test.docx",
+        extractor="soffice",
+    )
+    print(doc.content)
